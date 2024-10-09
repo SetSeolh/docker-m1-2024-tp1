29ac97854e9d2f62bd8e2e200720b06438f75f87
Formation <formation@localhost.localdomain>
2024-10-09 15:16:04 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.less'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <img src="/Docker-Symbol.png" className="logo" alt="Vite logo" />
+    </>
+  )
+}
+
+export default App
